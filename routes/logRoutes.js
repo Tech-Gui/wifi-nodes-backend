17b1223f906c24cdc15a603125ee50cab632971a
Isaiah Chiraira <itchiraira@gmail.com>
2026-04-30 02:20:30 +0200

    fix: authentication middleware import in logRoutes

--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const logController = require("../controllers/logController");
-const { authenticate } = require("../middleware/auth");
+const authenticate = require("../middleware/auth");
 
 router.get("/automation", authenticate, logController.getAutomationLogs);
 
